e4a7db2ad28034957800a2a6a6f5e44bf7fc4b32
tibbi <tibor@kaputa.sk>
2017-07-19 16:15:06 +0200

    adding the Gruveo sdk

--- app/src/main/java/com/gruveo/sdk/java/MainActivity.java
+++ app/src/main/java/com/gruveo/sdk/java/MainActivity.java
@@ -1,7 +1,10 @@
 package com.gruveo.sdk.java;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.gruveo.sdk.Gruveo;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.main_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Gruveo.Builder(MainActivity.this).callCode("hellofromsdk").clientId("demo").build();
+            }
+        });
     }
 }
