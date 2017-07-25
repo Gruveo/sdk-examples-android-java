c58b0b69cf691201e50dc7e5c8e043099d536030
tibbi <tibor@kaputa.sk>
2017-07-25 15:27:05 +0200

    add some UI to allow joining a custom call code as video/voice

--- app/src/main/java/com/gruveo/sdk/java/MainActivity.java
+++ app/src/main/java/com/gruveo/sdk/java/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 import com.gruveo.sdk.Gruveo;
 
@@ -14,32 +15,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.main_button).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.main_video_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final String result = new Gruveo.Builder(MainActivity.this).callCode("gruveorocks").clientId("demo").eventsListener(eventsListener).build();
-                switch (result) {
-                    case Gruveo.GRV_RES_MISSING_CALL_CODE: {
-                        break;
-                    }
-                    case Gruveo.GRV_RES_INVALID_CALL_CODE: {
-                        break;
-                    }
-                    case Gruveo.GRV_RES_MISSING_CREDENTIALS: {
-                        break;
-                    }
-                    case Gruveo.GRV_RES_INVALID_CREDENTIALS: {
-                        break;
-                    }
-                    case Gruveo.GRV_RES_OFFLINE: {
-                        break;
-                    }
-                    default: {
-                        break;
-                    }
-                }
+                initCall(true);
             }
         });
+
+        findViewById(R.id.main_voice_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                initCall(false);
+            }
+        });
+    }
+
+    private void initCall(Boolean videoCall) {
+        final String code = ((EditText) findViewById(R.id.main_edittext)).getText().toString();
+        final String result = new Gruveo.Builder(this).callCode(code).videoCall(videoCall).clientId("demo").eventsListener(eventsListener).build();
+        switch (result) {
+            case Gruveo.GRV_RES_MISSING_CALL_CODE: {
+                break;
+            }
+            case Gruveo.GRV_RES_INVALID_CALL_CODE: {
+                break;
+            }
+            case Gruveo.GRV_RES_MISSING_CREDENTIALS: {
+                break;
+            }
+            case Gruveo.GRV_RES_INVALID_CREDENTIALS: {
+                break;
+            }
+            case Gruveo.GRV_RES_OFFLINE: {
+                break;
+            }
+            default: {
+                break;
+            }
+        }
     }
 
     private Gruveo.EventsListener eventsListener = new Gruveo.EventsListener() {
